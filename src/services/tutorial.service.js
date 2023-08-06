26e431102ff7115e3d3ca024d0a1004c0630cf60
Baobaotv <nguyenbaowwe@gmail.com>
2023-08-07 00:47:39 +0700

    fix domain

--- src/services/tutorial.service.js
+++ src/services/tutorial.service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("/tutorials");
+    return http.get("https://bao.devops.ameladev.click/tutorials");
   }
 
   get(id) {
-    return http.get(`/tutorials/${id}`);
+    return http.get(`https://bao.devops.ameladev.click/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("/tutorials", data);
+    return http.post("https://bao.devops.ameladev.click/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
+    return http.put(`https://bao.devops.ameladev.click/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/tutorials/${id}`);
+    return http.delete(`https://bao.devops.ameladev.click/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/tutorials`);
+    return http.delete(`https://bao.devops.ameladev.click/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`/tutorials?title=${title}`);
+    return http.get(`https://bao.devops.ameladev.click/tutorials?title=${title}`);
   }
 }
 
