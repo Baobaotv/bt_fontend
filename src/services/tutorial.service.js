b34fca103b2775831429ff3b27411520d7cb99b4
Baobaotv <nguyenbaowwe@gmail.com>
2023-08-07 00:57:46 +0700

    fix call api

--- src/services/tutorial.service.js
+++ src/services/tutorial.service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("https://bao.devops.ameladev.click/tutorials");
+    return http.get("https://bao.devops.ameladev.click/api/tutorials");
   }
 
   get(id) {
-    return http.get(`https://bao.devops.ameladev.click/tutorials/${id}`);
+    return http.get(`https://bao.devops.ameladev.click/api/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("https://bao.devops.ameladev.click/tutorials", data);
+    return http.post("https://bao.devops.ameladev.click/api/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`https://bao.devops.ameladev.click/tutorials/${id}`, data);
+    return http.put(`https://bao.devops.ameladev.click/api/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`https://bao.devops.ameladev.click/tutorials/${id}`);
+    return http.delete(`https://bao.devops.ameladev.click/api/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`https://bao.devops.ameladev.click/tutorials`);
+    return http.delete(`https://bao.devops.ameladev.click/api/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`https://bao.devops.ameladev.click/tutorials?title=${title}`);
+    return http.get(`https://bao.devops.ameladev.click/api/tutorials?title=${title}`);
   }
 }
 
